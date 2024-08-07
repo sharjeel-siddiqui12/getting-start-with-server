703f7b04316312c0696afea74358d9c9f28a8f10
sharjeel-siddiqui12 <sharjeelsiddiqui00@gmail.com>
2024-08-08 02:51:42 +0500

    latest

--- server.mjs
+++ server.mjs
@@ -1,6 +1,6 @@
 import express from 'express'
 const app = express()
-const port = 3000
+const port = process.env.PORT || 3000
 
 app.get('/water', (req, res) => {
   console.log(`${req.ip} is asking for water`)
